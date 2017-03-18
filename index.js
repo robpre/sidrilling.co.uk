d9e6bc39df9445fa7c485bcdcd1c1a071e18fa76
Robert Preus-MacLaren <rpprroger@googlemail.com>
2017-03-18 12:38:09 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var Metalsmith = require('metalsmith');
+var collections = require('metalsmith-collections');
+var layouts = require('metalsmith-layouts');
+var markdown = require('metalsmith-markdown');
+var permalinks = require('metalsmith-permalinks');
+
+Metalsmith(__dirname) // __dirname defined by node.js:
+// name of current working directory
+.metadata({ // add any variable you want
+    // use them in layout-files
+    sitename: 'SIDrilling',
+    siteurl: 'http://example.com/',
+    description: ''
+})
+.source('./src') // source directory
+.destination('./build') // destination directory
+.clean(true) // clean destination before
+.use(collections({ // group all blog posts by internally
+    posts: 'posts/*.md' // adding key 'collections':'posts'
+})) // use `collections.posts` in layouts
+.use(markdown()) // transpile all md into html
+.use(permalinks({ // change URLs to permalink URLs
+    relative: false // put css only in /css
+}))
+.use(layouts({ // wrap layouts around html
+    engine: 'handlebars', // use the layout engine you like
+}))
+.build(function(err) { // build process
+    if (err) throw err; // error handling is required
+
+    console.log('built!');
+});
