c2d5e77507cb574a552e86aadebc90c9f7239d26
Robert Preus-MacLaren <rpprroger@gmail.com>
2022-09-07 11:22:23 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
     VERCEL_GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF || "",
     NOW_GITHUB_COMMIT_REF: process.env.NOW_GITHUB_COMMIT_REF || "",
   },
+  // While in a branch we need to match the vercel config
+  distDir: "build",
 };
 
 module.exports = nextConfig;
